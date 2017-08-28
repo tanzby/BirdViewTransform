1a9e73764e171c9a716fa1efc7d7b7e76fedeb3b
檀祖冰 <tanzby@outlook.com>
2017-08-28 19:23:17 +0800

    Update birdView.hpp

--- birdView.hpp
+++ birdView.hpp
@@ -331,32 +331,3 @@ private:
 	}
 };
 
-
-
-int main()
-{
-	Mat v[4];
-
-	for (int i = 0; i < 4; i++)
-	{
-		char buf[10];
-		sprintf(buf, "%d.png", i);
-		v[i] = imread(buf);
-	}
-
-
-	BirdView b("config.yml");
-
-	b.setCarSize(240, 380); 
-	b.setChessSize(60);
-	b.setMaskHeigth(200);
-	b.setInternalShift(27,27);
-
-	//b.sourcePointClick(v);
-
-	while (1)
-	{
-		imshow("bird view", b.transformView(v));
-		if (waitKey(20) == 27)	break;
-	}
-}
